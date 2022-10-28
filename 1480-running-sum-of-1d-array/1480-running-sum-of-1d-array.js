0fdc79e984480a75b67fc434ebcf9a19563c4942
Rade Mihovilovic <56633508+r4deadv@users.noreply.github.com>
2022-10-28 10:11:30 +0200

    Time: 97 ms (58.54%), Space: 42.3 MB (60.60%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let sum = [nums[0]]
+    for (let i = 1; i < nums.length; i++){
+        sum.push(sum[i - 1] + nums[i])
+    }
+    return sum;
+};
+
+// time complexity = O(n)
+// time complexity = O(n)
