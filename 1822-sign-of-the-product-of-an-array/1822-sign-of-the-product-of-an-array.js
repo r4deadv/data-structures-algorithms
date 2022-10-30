28276dccd2bc22e383abb34b16813a42d198c190
Rade Mihovilovic <56633508+r4deadv@users.noreply.github.com>
2022-10-30 14:39:26 +0100

    Time: 89 ms (72.69%), Space: 44.3 MB (17.58%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -3,6 +3,6 @@
  * @return {number}
  */
 var arraySign = function(nums) {
-    let product = nums.reduce((a, c) => a * c)
+    let product = nums.reduce((a, c) => a * c, 1)
     return product > 0 ? 1 : product < 0 ? -1 : 0
 };
