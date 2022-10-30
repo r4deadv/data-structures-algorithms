b5d817fad1b3188d92b042e0631088b79fe99dbc
Rade Mihovilovic <56633508+r4deadv@users.noreply.github.com>
2022-10-30 14:07:29 +0100

    Time: 100 ms (92.76%), Space: 50.5 MB (74.89%) - LeetHub

---
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number[][]} points
+ * @return {number}
+ */
+var nearestValidPoint = function(x, y, points) {
+    let distance = 0;
+    let smallestDistance = Infinity;
+    let result = -1;
+    points.forEach((point, i) => {
+        if(point[0] === x || point[1] === y) {
+            distance = Math.abs(point[0] - x) + Math.abs(point[1] - y)
+            if (distance < smallestDistance) {
+                smallestDistance = distance;
+                result = i;
+            }
+       }
+   })
+    return result
+};
