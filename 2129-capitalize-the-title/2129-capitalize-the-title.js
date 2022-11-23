81a036536b65e795572825a6235e86d8b857c7bb
Rade Mihovilovic <56633508+r4deadv@users.noreply.github.com>
2022-11-23 23:26:05 +0100

    Time: 73 ms (84.21%), Space: 42.3 MB (72.81%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.js
+++ 2129-capitalize-the-title/2129-capitalize-the-title.js
@@ -4,11 +4,6 @@
  */
 var capitalizeTitle = function(title) {
     let words = title.toLowerCase().split(' ');
-    return words.map(word => {
-        if(word.length > 2) {
-             return word.charAt(0).toUpperCase().concat(word.substring(1))
-        } else {
-            return word
-        }
-    }).join(' ')
+    return words.map(word => word.length > 2 ? word.charAt(0).toUpperCase().concat(word.substring(1)) : word
+    ).join(' ')
 };
