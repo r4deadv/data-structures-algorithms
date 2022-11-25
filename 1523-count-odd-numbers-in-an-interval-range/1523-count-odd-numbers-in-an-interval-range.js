b06159892baeaf672e3dbb0edfe005c4f59b67f9
Rade Mihovilovic <56633508+r4deadv@users.noreply.github.com>
2022-11-25 11:30:55 +0100

    Time: 72 ms (89.83%), Space: 41.7 MB (83.17%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -4,11 +4,6 @@
  * @return {number}
  */
 var countOdds = function(low, high) {
-    let count = 0;
-    for (let i = low; i <= high; i++) {
-        if(i % 2 !== 0) {
-            count ++
-        }
-    }
-    return count;
+    let count = Math.round((high - low) / 2)
+    return low % 2 !== 0 && high % 2 !== 0 ? count + 1 : count;
 };
