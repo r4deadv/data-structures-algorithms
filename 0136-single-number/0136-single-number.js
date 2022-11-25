80c49a9134e4e738093546da5a9083922ac8c15c
Rade Mihovilovic <56633508+r4deadv@users.noreply.github.com>
2022-11-25 13:03:55 +0100

    Time: 804 ms (8.73%), Space: 43 MB (91.40%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,5 +3,5 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    return nums.filter(i => nums.indexOf(i) === nums.lastIndexOf(i))
+    return nums.find(i => nums.indexOf(i) === nums.lastIndexOf(i))
 };
