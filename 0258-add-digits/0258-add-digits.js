c02f101169e0148dad49d5325d1739a8bf532fd9
Rade Mihovilovic <56633508+r4deadv@users.noreply.github.com>
2022-11-28 12:12:34 +0100

    Time: 123 ms (42.90%), Space: 45.6 MB (5.14%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,7 +3,10 @@
  * @return {number}
  */
 var addDigits = function(num) {
+    if(num < 10) {
+        return num;
+    }
     let sum = num.toString().split('').reduce((acc, curr) => Number(acc) + Number(curr), 0);
     console.log(sum)
-    return sum < 10 ? sum : addDigits(sum)
+    return addDigits(sum)
 };
